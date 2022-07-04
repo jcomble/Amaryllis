c097881d5a16f42a3cbaf49a4e6264af5a899021
jcomble <jcomble@live.fr>
2022-07-04 04:17:30 +0200

    help listing command updated

--- src/commands/Help.java
+++ src/commands/Help.java
@@ -33,7 +33,7 @@ public class Help {
 		embed.setDescription("Contacte DaSTOC-senpai pour des soucis techniques : <@931339802857586700> ");
 		embed.addField("Carte de présentation <a:hideri_hi:991710158386364437>", "`" + prefix + "me` : affiche ta carte de présentation\n`" + prefix + "renamemaster nom` : renomme le nom de ton maître\n`" + prefix + "changeurlmaster url` : change l'image du maître\n`" + prefix  + "changefetiche \"phrase\"` : change la phrase fétiche du maître\n`" + prefix + "changepseudosw \"pseudo\"` : change ton pseudo Switch\n `" + prefix + "changecasw codeami` : change ton code ami Switch", false);
 		embed.addField("Possessions <:hideri_joie:991706755069399070>", "`" + prefix + "inventory` : affiche ton inventaire\n`" + prefix + "familiers` : affiche tes familiers", false);
-		embed.addField("Personnalisation <:hideri_ahegao:991706750413717526>", "`" + prefix + "defineprefix` : définit le préfixe dans ce serveur", true);
+		embed.addField("Personnalisation <:hideri_ahegao:991706750413717526>", "`" + prefix + "defineprefix` : définit le préfixe dans ce serveur\n`" + prefix + "definecolor` : définit la couleur des embeds dans ce serveur", true);
 		embed.setFooter(prefix + "help");
 		channel.sendMessageEmbeds(embed.build()).queue();
 	}
